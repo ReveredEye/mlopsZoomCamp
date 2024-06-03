c0bd4fb35e306210ed9845ce9178e8812b7cb2ed
ReveredEye <wylie.yang19@gmail.com>
2024-06-03 05:10:26 +0000

    mage isn't using proper mlflow. can't even print version

--- your_first_project/custom/lr_model.py
+++ your_first_project/custom/lr_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_error
+import mlflow
 
 if 'custom' not in globals():
     from mage_ai.data_preparation.decorators import custom
@@ -18,13 +19,23 @@ def transform_custom(train_data, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your custom logic here
-    featDict = train_data[['PULocationID', 'DOLocationID']].to_dict(orient = 'records')
-    dv = DictVectorizer()#sparse = False <- Use if small dataset
-    dv.fit(featDict)
-    featMat = dv.transform(featDict)
-    lr_model = LinearRegression()
-    lr_model.fit(featMat, train_data['duration'])
-    print(lr_model.intercept_)
+    print(mlflow.__version__)
+    mlflow.set_tracking_uri('sqlite:///home/mlflow/mlflow.db')
+    mlflow.set_experiment('nyc-yellow-23-lrm')
+
+    # Below sets log_datasets = False to not track training data as data is NumPy array but
+    # mlflow expects pandas DataFrames.
+    mlflow.sklearn.autolog(log_datasets = False)
+
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "wylie")
+        featDict = train_data[['PULocationID', 'DOLocationID']].to_dict(orient = 'records')
+        dv = DictVectorizer()#sparse = False <- Use if small dataset
+        dv.fit(featDict)
+        featMat = dv.transform(featDict)
+        lr_model = LinearRegression()
+        lr_model.fit(featMat, train_data['duration'])
+        print(lr_model.intercept_)
 
     return dv, lr_model
 
